b09d807e6ef646e2480329fd278bcf7b31a95bb7
amirisback <faisalamircs@gmail.com>
2022-08-13 00:18:36 +0700

    update: SVG

--- app/src/main/java/com/frogobox/research/MainActivity.kt
+++ app/src/main/java/com/frogobox/research/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.frogobox.research
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
+import com.frogobox.research.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val BASE_URL_SVG = ""
+        Glide.with(this)
+            .setDefaultRequestOptions(RequestOptions().timeout(60000))
+            .load(BASE_URL_SVG)
+            .disallowHardwareConfig()
+            .override(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+
+            })
+            .into(binding.ivIcon)
+
+
     }
 }
